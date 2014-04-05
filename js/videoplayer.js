64e0a381ee22472826a82138aa860d33058db797
martin <mjschwan@gmx.de>
2014-04-05 19:39:37 +0200

    added favicon, basic seek slider for videos

--- js/videoplayer.js
+++ js/videoplayer.js
@@ -17,14 +17,50 @@ $(document).ready(function() {
         class: "fa fa-pause"
     });
     
-    $video.each(function() {
-        $(this)[0].removeAttribute("controls");
+    $seek.slider({
+        min: 0,
+        max: 100,
+        step: 0.01,
+        value: 0,
+        range: "min",
+        animate: "fast",
+        slide: function(event, ui) {
+            $(this).closest(".widget-video-controls").find(".widget-video-endtime").text(ui.value);
+            $(this).closest(".widget-video").find("video")[0].currentTime = ui.value;
+        }
     });
     
-    $seek.each(function() {
-        $(this).css("background-color", "#888")
-                .css("color","#fff");
+    $video.each(function() {
+        $(this)[0].removeAttribute("controls");
+        $(this)[0].addEventListener("timeupdate", function() {
+            $(this).closest(".widget-video").find(".widget-video-currenttime").text($(this)[0].currentTime);
+            $(this).closest(".widget-video").find(".widget-video-seek").slider("value", $(this)[0].currentTime);
+        });
     });
+    
+    
+    /*var offset;
+    var relativePosition;
+    var width;
+    var rect;
+    var maxWidth;
+    var cssWidth;
+    
+    $seek.on("click", function(event) {
+        rect = $(this)[0].getBoundingClientRect();
+        maxWidth = Math.round(rect.right - rect.left);
+        
+        var currenttime = $(this).closest(".widget-video").find("video")[0].duration;
+        $(this).closest(".widget-video-controls").find(".widget-video-currenttime").text(currenttime);
+
+        offset = $(this).offset();
+        relativePosition = Math.round(event.pageX - offset.left);
+
+        width = Math.round((relativePosition / maxWidth) * 100);
+        cssWidth = width + "%";
+        $(this).closest(".widget-video-controls").find(".widget-video-endtime").text(width);
+        $(this).find(".widget-video-seek-played").css("width", cssWidth);
+    });*/
     
     $playpause.on("click", function() {
         if($(this).closest(".widget-video-controls").siblings("video")[0].paused) {
